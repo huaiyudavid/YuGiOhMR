aff533e18449310f5bedc93cef5d60e8a8781d30
David K. Zhang <dzhang314@gmail.com>
2018-07-22 00:43:28 -0700

    Put monster field offsets in a public array

--- Assets/Scripts/ProceduralPlayFieldBehavior.cs
+++ Assets/Scripts/ProceduralPlayFieldBehavior.cs
@@ -2,12 +2,20 @@
 
 public class ProceduralPlayFieldBehavior : MonoBehaviour {
 
-    const float cardWidth = 1.0F;
-    const float cardHeight = 1.345F;
+    public const float cardWidth = 1.0F;
+    public const float cardHeight = 1.345F;
 
-    public float fieldPadding = 0.1F;
-    public float interFieldPadding = 0.2F;
-    public float fieldThickness = 0.02F;
+    public const float fieldPadding = 0.1F;
+    public const float interFieldPadding = 0.2F;
+    public const float fieldThickness = 0.02F;
+
+    public static Vector3[] monsterFieldOffsets = new Vector3[5] {
+        -2.0F * cardHeight * Vector3.right,
+        -cardHeight * Vector3.right,
+        Vector3.zero,
+        +cardHeight * Vector3.right,
+        +2.0F * cardHeight * Vector3.right,
+    };
 
     public GameObject[] CreatePlayFieldFrame(Transform parent, Vector3 offset) {
         float x = 0.5F * cardWidth + fieldPadding;
@@ -23,11 +31,9 @@ public class ProceduralPlayFieldBehavior : MonoBehaviour {
     // Use this for initialization
     void Start() {
         GameObject parent = new GameObject();
-        CreatePlayFieldFrame(parent.transform, Vector3.zero);
-        CreatePlayFieldFrame(parent.transform, new Vector3(+cardHeight, 0.0F));
-        CreatePlayFieldFrame(parent.transform, new Vector3(-cardHeight, 0.0F));
-        CreatePlayFieldFrame(parent.transform, new Vector3(+2.0F * cardHeight, 0.0F));
-        CreatePlayFieldFrame(parent.transform, new Vector3(-2.0F * cardHeight, 0.0F));
+        foreach (Vector3 offset in monsterFieldOffsets) {
+            CreatePlayFieldFrame(parent.transform, offset);
+        }
         RotateBehavior rotate = parent.AddComponent<RotateBehavior>();
         rotate.RotationSpeed = 30.0F;
         rotate.RotationDirection = Random.onUnitSphere;
